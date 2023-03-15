935c3f5eeb16619fdc677626ee018a7bb7acb73e
Islembelmenigher <90978085+Islembelmenigher@users.noreply.github.com>
2023-03-15 11:42:53 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    cout<<"type your name :"<<endl;
+          cin>>name;
+          cout<<"hello mister "<<name<<endl;
+    return 0;
+}
